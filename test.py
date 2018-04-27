0526e897ce956dac3f0c3ba5e01dc1ef1fbb6202
LIU\Adven <kcaj1992@gmail.com>
2018-04-27 17:21:07 +0800

    test.py

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
-print("I'm tired")
-print("1*2 is ")
-print(1*2)
-
 #git add .
 #git status
 #git commit -m "filemame"
 #git status
 #git push origin master
 #git status
+
+s= "Stone Campus"
+print('Stone Campus is long' ,len(s))
+print(s[6])
+print(s[3:10])
+print(s[3:])
+print(s[:10])
